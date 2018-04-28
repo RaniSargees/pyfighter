9dbad5c37441ed23c657657defd6137aad162df6
Ryota Parsons <Ryotaparsons2@gmail.com>
2018-04-28 18:39:20 -0400

    Added Vertical attacks. Fixed bug where a character would slide instead of being knocked up

--- Characters.py
+++ Characters.py
@@ -20,10 +20,15 @@ class Char(pygame.sprite.Sprite):
 		self.hspeed = 0
 		self.gravity = 32
 		self.gravityMultiplier = 1
-		self.dmg = 0
 		self.joystick = joystick
+		self.dmg = 0
 		self.stun = 0
-		self.face_left = 0
+		self.knocked = 0
+		self.facing = 0
+		#0 = Left
+		#1 = Right
+		#2 = Up
+		#3 = Down
 		self.hitbox = (self.x+4,self.y+4,40,64)
 	def update(self,keys,events):
 		self.hitbox = (self.x+4,self.y+4,40,64)
@@ -31,9 +36,12 @@ class Char(pygame.sprite.Sprite):
 		self.events = events
 		self.grounded = (self.y >= self.game.ground)
 		if self.grounded:
-			self.currentJumps = self.maxJumps
-			self.y = self.game.ground
-			self.vspeed = 0
+			if self.knocked:
+				self.vspeed *= -1
+			else:
+				self.currentJumps = self.maxJumps
+				self.y = self.game.ground
+				self.vspeed = 0
 		else: self.vspeed += self.gravity * self.gravityMultiplier
 		self.get_keys()
 		self.y += self.vspeed * self.game.dt
@@ -47,38 +55,55 @@ class Char(pygame.sprite.Sprite):
 			self.jumpBonus = 0
 
 	def atkLight(self):
-		pygame.draw.rect(self.game.win,RED,(self.x-20+((self.face_left==0)*(48+20)), self.y, 20, 72),4)
-		collisions=[(pygame.Rect((self.x-20+((self.face_left==0)*(48+20)),self.y,20,72)).colliderect(x.hitbox),x)for x in self.game.sprites]
-		[x[1].knockBack(50*-(self.face_left-.5)*2) for x in collisions if x[0]]
+		if self.facing < 2:
+			pygame.draw.rect(self.game.win,BLUE,(self.x-20+((self.facing==1)*(48+20)), self.y, 20, 72),4)
+			collisions=[(pygame.Rect((self.x-20+((self.facing==1)*(48+20)),self.y,20,72)).colliderect(x.hitbox),x)for x in self.game.sprites]
+		elif self.facing >= 2:
+			pygame.draw.rect(self.game.win,BLUE,(self.x,self.y-20+((self.facing==3)*(72+10)),48,30),4)
+			collisions=[(pygame.Rect((self.x,self.y-20+((self.facing==3)*(72+10)),48,30)).colliderect(x.hitbox),x)for x in self.game.sprites]
+		[x[1].knockBack(20,self.facing) for x in collisions if x[0] and not(x[1] == self)]
 	def atkHeavy(self):self.atkLight()
 
-	def knockBack(self,hit):
+	def knockBack(self,hit,direction=0):
+		#direction represents the direction of the attacking player
 		self.stun = hit/200
-		vel = (self.dmg+abs(hit))**1.5
-		self.hspeed = (hit/abs(hit))*vel*math.cos(math.pi/6)
-		self.vspeed = -vel*math.sin(math.pi/6)
+		self.knocked = 1
+		vel = (self.dmg+hit)**1.5
+		if direction < 2:
+			self.hspeed = (direction-0.5)*2*vel*math.cos(math.pi/6)
+			self.vspeed = -vel*math.sin(math.pi/6)
+			print(self.vspeed)
+		elif direction >= 2:
+			self.vspeed = ((direction==3)-0.5)*2*vel*math.sin(math.pi/4)
 		self.currentJumps = max(1, self.currentJumps)
-		self.dmg+=abs(hit/4)
-		print(self.dmg)
+		self.dmg+=hit/4
+		#Don't remove the line below. It fixes the sliding bug
+		self.y -= 5
 
 	def get_keys(self):
 		if self.stun <= 0:
+			self.knocked = 0
 			if  self.joystick.get_axis(0) < -.5 and -self.hspeed < self.maxMoveSpeed:
 				self.hspeed -=self.moveSpeed
-				self.face_left = 1
+				self.facing = 0
 			elif self.joystick.get_axis(0) > .5 and self.hspeed < self.maxMoveSpeed:
 				self.hspeed += self.moveSpeed
-				self.face_left = 0
+				self.facing = 1
 			elif self.hspeed: self.hspeed -= self.hspeed/abs(self.hspeed)*min(self.moveSpeed, abs(self.hspeed))
-			self.gravityMultiplier = (self.joystick.get_axis(1)>.75)*2+1
+			if self.joystick.get_axis(1)>.75:
+				self.facing = 3
+			elif self.joystick.get_axis(1)<-.75:
+				self.facing = 2
+			self.gravityMultiplier = (self.joystick.get_axis(1) >.75)*2 + 1
+			if self.joystick.get_button(0) and (self.vspeed < 0) and (self.jumpBonus < self.maxJumpBonus):
+				self.vspeed += self.jumpBonusSpeed
+				self.jumpBonus += 1
 			for e in self.events:
-				if e.type == pygame.KEYDOWN and e.key == pygame.K_p:self.knockBack(60) #testing only, remove later
+				if e.type == pygame.KEYDOWN and e.key == pygame.K_p:self.knockBack(60,self.facing) #testing only, remove later
 				if e.type == pygame.JOYBUTTONDOWN and e.joy==self.joystick.get_id():
 					if e.button == 0: self.jump()
 					if e.button == 1: self.atkLight()
 					if e.button == 2: self.atkHeavy()
-			if self.joystick.get_button(0) and (self.vspeed < 0) and (self.jumpBonus < self.maxJumpBonus):
-				self.vspeed += self.jumpBonusSpeed
-				self.jumpBonus += 1
+
 		else:
 			self.stun -= self.game.dt
--- Game.py
+++ Game.py
@@ -39,4 +39,5 @@ class Game():
 g = Game()
 g.new()
 g.run()
+pygame.quit()
 
