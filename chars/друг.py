c9d4031ee69ec1c83d532ebe694d2cf0ef77d240
Ryota Parsons <Ryotaparsons2@gmail.com>
2018-06-13 11:15:56 -0400

    started work on SP3 for друг

--- chars/друг.py
+++ chars/друг.py
@@ -1,4 +1,5 @@
 import pygame
+from colorsys import hls_to_rgb
 from settings import *
 from modules.character import *
 from modules.projectiles import *
@@ -46,4 +47,27 @@ class char(Char):
 		self.vspeed = -800
 		self.gravityMultiplier = 12
 		[rainbow_poop(self,self.x,self.y-40,randint(0,1),yspeed=5,xspeed=uniform(0,.25),bounce=1)for x in".."]
+	
+	def special3(self):
+		if not(self.ability_run+1):
+			self.release = 0
+			self.ability_run = 3
+			self.ability_time = -1
+			self.hspeed = 0
+			self.freeze = 2
+			self.count = 0
+			self.hue = 0
+	
+	def run_special3(self):
+		self.color = [int(x*255) for x in hls_to_rgb(self.hue%360,0.5,1)]
+		print(hls_to_rgb(self.hue%360,0.5,1))
+		self.hue+=3
+		pygame.draw.polygon(self.game.win,self.color,((self.x-7,self.y-79),(self.x-10,self.y-72),(self.x-7,self.y-65),(self.x+7,self.y-65),(self.x+10,self.y-72),(self.x+7,self.y-79)))
+		
+		
+		if self.release:
+			self.ability_run = 0
+			self.ability_time = 0
+			self.stun = 0.4
+		
 
