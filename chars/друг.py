f02baafa5f7dfe82516dbba99a9c4ea307b109c5
Ryota Parsons <Ryotaparsons2@gmail.com>
2018-06-13 20:14:32 -0400

    Added collision to друг's SP3

--- chars/друг.py
+++ chars/друг.py
@@ -90,9 +90,13 @@ class char(Char):
 		pygame.draw.polygon(self.surf,WHITE,((180,0),(180,180),(120,180)))
 		self.surf.set_colorkey((WHITE))
 		self.game.win.blit(self.surf,(self.x-90,self.y-180))
+		
+		collisions=[(pygame.Rect((self.x-90,self.y-180,180,180)).colliderect(x.hitbox),x)for x in self.game.sprites]
+		[(x[1].knockBack(10, randint(0,2)),x[1].damage(0.5*self.attack))for x in collisions if x[0] and not(x[1]==self)]
+		
 			
 			
-"""
+""" Reuse somewhere else I guess
 	def special3(self):
 		if not(self.ability_run+1):
 			self.release = 0
