48d994cd5c1c5171dc326bf124ce1d75910fa602
az4521 <cheeze.sprinkels+official_git@gmail.com>
2018-06-15 10:54:31 -0400

    added hand walking animation

--- modules/animation.py
+++ modules/animation.py
@@ -64,10 +64,17 @@ class animator():
 		l_leg, ll_rect = animator.pivot(self.l_leg, l_angle, (128-21+15,256-45), (-7.5,15))
 		r_arm, ra_rect = animator.pivot(self.r_arm, l_angle/2+90, (128+17,256-81), (15,-7.5))
 		l_arm, la_rect = animator.pivot(self.l_arm, r_angle/2-90, (128-32+15,256-81), (-15,-7.5))
+		l_hand_offset = pygame.math.Vector2(-28,0).rotate(r_angle/2-90) #generate offsets for hand positions
+		r_hand_offset = pygame.math.Vector2(28,0).rotate(l_angle/2+90)
+		r_hand, rh_rect = animator.pivot(self.r_hand,l_angle/2+90, r_hand_offset+(128+17,256-81), (7.5,-7.5)) #rotate hands about connecting point
+		l_hand, lh_rect = animator.pivot(self.l_hand,r_angle/2-90, l_hand_offset+(128-32+15,256-81), (-7.5,-7.5))
+
 		self.surface.blit(r_leg, rl_rect) #blit leg
+		self.surface.blit(r_hand, rh_rect.move(0,abs(r_angle/3))) #blit hand
 		self.surface.blit(r_arm, ra_rect.move(0,abs(r_angle/3))) #blit arm (bobbing w/ body)
 		self.surface.blit(self.head, (128-10.5, 256-120 + abs(r_angle/3))) #blit body and head (bobbing)
 		self.surface.blit(self.body, (128-  21, 256- 99 + abs(r_angle/3)))
 		self.surface.blit(l_leg, ll_rect) #blit leg
+		self.surface.blit(l_hand, lh_rect.move(0,abs(r_angle/3))) #blit hand
 		self.surface.blit(l_arm, la_rect.move(0,abs(r_angle/3))) #blit arm (bobbing w/ body)
 		return self.surface
