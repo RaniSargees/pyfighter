cdd510b921c4e0e9f013e80833de7ca9e0bd1bec
Ryota Parsons <Ryotaparsons2@gmail.com>
2018-06-15 21:03:40 -0400

    Added SP3 to brawler Its a reflector

--- chars/brawler.py
+++ chars/brawler.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from math import sin, radians
 from modules.character import *
 from modules.projectiles import *
 
@@ -41,12 +42,45 @@ class char(Char):
 			self.freeze = 2
 			self.count = 0
 			self.hue = 0
+			self.open = 0
+			self.start = 1
+			self.end = 0
+			self.hit_list
 
 	def run_special3(self):
+		if self.start:
+			self.open += 1
+			if self.open >= 90:
+				self.open = 90
+				self.start = False
+		elif self.end:
+			self.open -= 3
+			if self.open <= 0:
+				self.open = 0
+				self.ability_run = 0
+				self.ability_time = 0
+			
 		self.color = [int(x*255) for x in hls_to_rgb(self.hue/360,0.5,1)]
 		self.hue+=1
+		if self.hue%30 == 0:
+			self.hit_list = []
 		pygame.draw.polygon(self.game.win,self.color,((self.x-7,self.y-79),(self.x-10,self.y-72),(self.x-7,self.y-65),(self.x+7,self.y-65),(self.x+10,self.y-72),(self.x+7,self.y-79)))
+		pygame.draw.lines(self.game.win,self.color,1,((self.x-(70*sin(radians(self.hue*4))),self.y-72-(70*sin(radians(self.open)))),
+													(self.x-(100*sin(radians(self.hue*4))),self.y-72),
+													(self.x-(70*sin(radians(self.hue*4))),self.y-72+(70*sin(radians(self.open)))),
+													(self.x+(70*sin(radians(self.hue*4))),self.y-72+(70*sin(radians(self.open)))),
+													(self.x+(100*sin(radians(self.hue*4))),self.y-72),
+													(self.x+(70*sin(radians(self.hue*4))),self.y-72-(70*sin(radians(self.open))))),10)
+		collisions=[(pygame.Rect((self.x-70,self.y-142,140,142)).colliderect(x.hitbox),x)for x in self.game.objects]
+		for x in collisions:
+			if x[0] and not(x[1] in self.hit_list):
+				x[1].Xspeed *= -1
+				x[1].Yspeed *= -1
+				x[1].hit_list = [self]
+				try: x[1].facing = (x[1].facing==0)
+				except: pass
+				
+		self.hit_list.extend([x[1] for x in collisions if x[0] and not(x[1] in self.hit_list)])
 		if self.release:
-			self.ability_run = 0
-			self.ability_time = 0
-			self.stun = 0.4
+			self.start = False
+			self.end = True
--- modules/projectiles.py
+++ modules/projectiles.py
@@ -12,13 +12,16 @@ class fireball(pygame.sprite.Sprite):
 		self.hitTarget = 0
 		self.count = 0
 		self.go = 0
+		self.Xspeed = 20
+		self.Yspeed = 0
 		self.explosion = self.char.explosion
 		self.hit_list = [self,char]
 	def update(self):
+		self.hitbox = (self.loc[0]+50,self.loc[1]+50,100,100)
 		if self.hitTarget and not(self.count):
 			self.go = 1
 		elif not(self.count):
-			self.loc = (self.loc[0] + (self.dir-0.5)*20,self.loc[1])
+			self.loc = (self.loc[0] + (self.dir-0.5)*self.Xspeed,self.loc[1])
 			collisions = [(pygame.Rect((self.loc[0]+85,self.loc[1]+85,30,30)).colliderect(x.hitbox),x)for x in self.char.game.sprites]
 			collisions.extend([(pygame.Rect((self.loc[0]+85,self.loc[1]+85,30,30)).colliderect(x.rect),x)for x in self.char.game.ground if not x.platform])
 			self.hitTarget = bool(len([x[1] for x in collisions if x[0] and not(x[1] in self.hit_list)]))
@@ -43,7 +46,7 @@ class rainbow_poop(pygame.sprite.Sprite):
 	def __init__(self, char, x, y, direction, yspeed = -5, xspeed = 2, bounce = 0):
 		pygame.sprite.Sprite.__init__(self, char.game.objects)
 		self.char = char
-		self.xspeed= xspeed
+		self.Xspeed= xspeed
 		self.x = x-24
 		self.y = y-24
 		self.bounce = bounce
@@ -54,7 +57,7 @@ class rainbow_poop(pygame.sprite.Sprite):
 		self.loop_len = len(self.image)
 		self.frame = 0
 		hue = random()*360
-		self.yspeed = yspeed
+		self.Yspeed = yspeed
 		for x in self.image:
 			pxarray = pygame.PixelArray(x)
 			pxarray.replace((255,  0, 0), tuple([int(255*x) for x in hls_to_rgb(hue, .5,  1)]))
@@ -64,21 +67,22 @@ class rainbow_poop(pygame.sprite.Sprite):
 			del pxarray
 
 	def update(self):
+		self.hitbox = (self.x+20, self.y+20,8,8)
 		self.frame += 1
 		self.char.game.win.blit(self.image[self.frame%self.loop_len],(self.x, self.y))
-		self.x += (self.dir*2-1) * self.xspeed * 800 * self.char.game.dt
-		self.y += self.yspeed
-		self.yspeed += 1
+		self.x += (self.dir*2-1) * self.Xspeed * 800 * self.char.game.dt
+		self.y += self.Yspeed
+		self.Yspeed += 1
 		collisions=[(pygame.Rect(x.hitbox).collidepoint(self.x+24, self.y+24), x)for x in self.char.game.sprites]
 		collisions=[x[1].damage(1*self.char.attack) for x in collisions[:]if x[1]not in self.hit_list and x[0]]
 		if self.y < -500 or self.x > 2080 or self.x < -800 or self.y > 1000 or len(collisions):
 			self.kill()
 		elif  [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.x+18,self.y+18,12,12)]:
 			if not self.bounce:self.kill()
-			else:self.bounce-=1;self.yspeed*=-.8;self.xspeed*=.8
+			else:self.bounce-=1;self.Yspeed*=-.8;self.Xspeed*=.8
 
 class Tinylaser(pygame.sprite.Sprite):
-	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0):
+	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0,hit_list=None):
 		pygame.sprite.Sprite.__init__(self, char.game.objects)
 		self.char = char
 		self.Xspeed = Xspeed
@@ -86,20 +90,23 @@ class Tinylaser(pygame.sprite.Sprite):
 		self.loc = (x,y)
 		self.image = pygame.transform.rotate(pygame.transform.scale(self.char.bullet[0],(20,15)),angle)
 		self.image_rect = self.image.get_rect()
+		if hit_list == None: self.hit_list = self.char
+		else: self.hit_list = hit_list
+		self.hitbox = (0,0,0,0)
 
 	def update(self):
 		self.char.game.win.blit(self.image,self.loc)
 		self.loc = (self.loc[0]+self.Xspeed,self.loc[1]+self.Yspeed)
-		self.collision_rect = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
-		collisions=[(pygame.Rect(x.hitbox).colliderect(self.collision_rect), x) for x in self.char.game.sprites]
-		collisions=[x[1].damage(1*self.char.attack) for x in collisions[:]if x[1]!=self.char and x[0]]
+		self.hitbox = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
+		collisions=[(pygame.Rect(x.hitbox).colliderect(self.hitbox), x) for x in self.char.game.sprites]
+		collisions=[x[1].damage(1*self.char.attack) for x in collisions[:]if not(x[1] in self.hit_list) and x[0]]
 		if self.loc[1] < -500 or self.loc[0] > 2080 or self.loc[0] < -800 or self.loc[1] > 1000 or len(collisions):
 			self.kill()
-		elif [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.collision_rect) and x.platform == 0]:
+		elif [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.hitbox) and x.platform == 0]:
 			self.kill()
 
 class laser(pygame.sprite.Sprite):
-	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0):
+	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0,hit_list=None):
 		pygame.sprite.Sprite.__init__(self, char.game.objects)
 		self.char = char
 		self.Xspeed = Xspeed
@@ -107,20 +114,23 @@ class laser(pygame.sprite.Sprite):
 		self.loc = (x,y)
 		self.image = pygame.transform.rotate(pygame.transform.scale(self.char.bullet[0],(40,30)),angle)
 		self.image_rect = self.image.get_rect()
+		if hit_list == None: self.hit_list = [self.char]
+		else: self.hit_list = [hit_list]
+		self.hitbox = (0,0,0,0)
 
 	def update(self):
 		self.char.game.win.blit(self.image,self.loc)
 		self.loc = (self.loc[0]+self.Xspeed,self.loc[1]+self.Yspeed)
-		self.collision_rect = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
-		collisions=[(pygame.Rect(x.hitbox).colliderect(self.collision_rect), x) for x in self.char.game.sprites]
-		collisions=[x[1].damage(3*self.char.attack) for x in collisions[:]if x[1]!=self.char and x[0]]
+		self.hitbox = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
+		collisions=[(pygame.Rect(x.hitbox).colliderect(self.hitbox), x) for x in self.char.game.sprites]
+		collisions=[x[1].damage(3*self.char.attack) for x in collisions[:]if not(x[1] in self.hit_list) and x[0]]
 		if self.loc[1] < -500 or self.loc[0] > 2080 or self.loc[0] < -800 or self.loc[1] > 1000 or len(collisions):
 			self.kill()
-		elif [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.collision_rect) and x.platform == 0]:
+		elif [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.hitbox) and x.platform == 0]:
 			self.kill()
 
 class BIGlaser(pygame.sprite.Sprite):
-	def __init__(self,char,x,y,Xspeed,Yspeed,angle,facing):
+	def __init__(self,char,x,y,Xspeed,Yspeed,angle,facing,hit_list=None):
 		pygame.sprite.Sprite.__init__(self, char.game.objects)
 		self.facing = facing
 		self.char = char
@@ -129,20 +139,23 @@ class BIGlaser(pygame.sprite.Sprite):
 		self.loc = (x,y)
 		self.image = pygame.transform.rotate(self.char.bullet[0],angle)
 		self.image_rect = self.image.get_rect()
+		if hit_list == None: self.hit_list = [self.char]
+		else: self.hit_list = [hit_list]
+		self.hitbox = (0,0,0,0)
 
 	def update(self):
 		self.char.game.win.blit(self.image,self.loc)
 		self.loc = (self.loc[0]+self.Xspeed,self.loc[1]+self.Yspeed)
-		self.collision_rect = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
-		collisions=[(pygame.Rect(x.hitbox).colliderect(self.collision_rect), x) for x in self.char.game.sprites]
-		collisions=[(x[1].knockBack(40*self.char.attack,self.facing),x[1].damage(15*self.char.attack)) for x in collisions[:]if x[1]!=self.char and x[0]]
+		self.hitbox = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
+		collisions=[(pygame.Rect(x.hitbox).colliderect(self.hitbox), x) for x in self.char.game.sprites]
+		collisions=[(x[1].knockBack(40*self.char.attack,self.facing),x[1].damage(15*self.char.attack)) for x in collisions[:]if not(x[1] in self.hit_list) and x[0]]
 		if self.loc[1] < -500 or self.loc[0] > 2080 or self.loc[0] < -800 or self.loc[1] > 1000 or len(collisions):
 			self.kill()
-		elif [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.collision_rect) and x.platform == 0]:
+		elif [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.hitbox) and x.platform == 0]:
 			self.kill()
 
 class Bomblaser(pygame.sprite.Sprite):
-	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0,facing=0):
+	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0,facing=0,hit_list=None):
 		pygame.sprite.Sprite.__init__(self, char.game.objects)
 		self.char = char
 		self.facing = facing
@@ -152,26 +165,29 @@ class Bomblaser(pygame.sprite.Sprite):
 		self.angle = angle
 		self.image = pygame.transform.rotate(self.char.bullet[0],angle)
 		self.image_rect = self.image.get_rect()
+		if hit_list == None: self.hit_list = [self.char]
+		else: self.hit_list = [hit_list]
+		self.hitbox = (0,0,0,0)
 
 	def update(self):
 		self.char.game.win.blit(self.image,self.loc)
 		self.loc = (self.loc[0]+self.Xspeed,self.loc[1]+self.Yspeed)
 		self.Yspeed += 0.5
-		self.collision_rect = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
-		collisions=[(pygame.Rect(x.hitbox).colliderect(self.collision_rect), x) for x in self.char.game.sprites]
-		collisions=[(x[1].knockBack(18*self.char.attack,self.facing),x[1].damage(30*self.char.attack)) for x in collisions[:]if x[1]!=self.char and x[0]]
+		self.hitbox = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
+		collisions=[(pygame.Rect(x.hitbox).colliderect(self.hitbox), x) for x in self.char.game.sprites]
+		collisions=[(x[1].knockBack(18*self.char.attack,self.facing),x[1].damage(30*self.char.attack)) for x in collisions[:]if not(x[1] in self.hit_list) and x[0]]
 		self.angle += 4
 		self.image = pygame.transform.rotate(self.char.bullet[0],self.angle)
 		self.image_rect = self.image.get_rect()
-		if [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.collision_rect) and x.platform == 0] or len(collisions):
+		if [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.hitbox) and x.platform == 0] or len(collisions):
 			for i in [[0.707,0.707,-45],[0,1,-90],[-0.707,0.707,-135],[-0.707,-0.707,-180],[-1,0,135],[0,-1,90],[0.707,-0.707,45],[1,0,0]]:
-				M_Bomblaser(self.char,self.loc[0],self.loc[1],i[0]*10,(i[1]+1)*-10,angle = i[2],facing=self.facing)
+				M_Bomblaser(self.char,self.loc[0],self.loc[1],i[0]*10,(i[1]+1)*-10,angle = i[2],facing=self.facing,hit_list = self.hit_list)
 			self.kill()
 		elif self.loc[1] < -500 or self.loc[0] > 2080 or self.loc[0] < -800 or self.loc[1] > 1000:
 			self.kill()
 
 class M_Bomblaser(pygame.sprite.Sprite):
-	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0,facing=0):
+	def __init__(self,char,x,y,Xspeed,Yspeed,angle=0,facing=0,hit_list=None):
 		pygame.sprite.Sprite.__init__(self, char.game.objects)
 		self.char = char
 		self.facing = facing
@@ -182,20 +198,23 @@ class M_Bomblaser(pygame.sprite.Sprite):
 		self.transformed_image = pygame.transform.scale(self.char.bullet[0],(40,30))
 		self.image = pygame.transform.rotate(self.transformed_image,angle)
 		self.image_rect = self.image.get_rect()
+		if hit_list == None: self.hit_list = [self.char]
+		else: self.hit_list = [hit_list]
+		self.hitbox = (0,0,0,0)
 
 	def update(self):
 		self.char.game.win.blit(self.image,self.loc)
 		self.loc = (self.loc[0]+self.Xspeed,self.loc[1]+self.Yspeed)
 		self.Yspeed += 0.5
-		self.collision_rect = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
-		collisions=[(pygame.Rect(x.hitbox).colliderect(self.collision_rect), x) for x in self.char.game.sprites]
-		collisions=[(x[1].knockBack(4*self.char.attack,self.facing),x[1].damage(5*self.char.attack)) for x in collisions[:]if x[1]!=self.char and x[0]]
+		self.hitbox = (self.loc[0]+self.image_rect[2]//4,self.loc[1]+self.image_rect[3]//4,self.image_rect[2]//2,self.image_rect[3]//2)
+		collisions=[(pygame.Rect(x.hitbox).colliderect(self.hitbox), x) for x in self.char.game.sprites]
+		collisions=[(x[1].knockBack(4*self.char.attack,self.facing),x[1].damage(5*self.char.attack)) for x in collisions[:]if not(x[1] in self.hit_list) and x[0]]
 		self.angle += 4
 		self.image = pygame.transform.rotate(self.transformed_image ,self.angle)
 		self.image_rect = self.image.get_rect()
-		if [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.collision_rect) and x.platform == 0] or len(collisions):
+		if [x for x in self.char.game.ground if pygame.Rect(x.rect).colliderect(self.hitbox) and x.platform == 0] or len(collisions):
 			for i in [[0.707,0.707,-45],[0,1,-90],[-0.707,0.707,-135],[-0.707,-0.707,-180],[-1,0,135],[0,-1,90],[0.707,-0.707,45],[1,0,0]]:
-				Tinylaser(self.char,self.loc[0],self.loc[1],i[0]*40,i[1]*40,i[2])
+				Tinylaser(self.char,self.loc[0],self.loc[1],i[0]*40,i[1]*40,i[2],hit_list = self.hit_list)
 			self.kill()
 		elif self.loc[1] < -500 or self.loc[0] > 2080 or self.loc[0] < -800 or self.loc[1] > 1000:
 			self.kill()
